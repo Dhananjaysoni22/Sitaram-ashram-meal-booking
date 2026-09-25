e2421e2e55ed23e293ca53375c5253c1bec02672
Dhananjaysoni22 <96926507+Dhananjaysoni22@users.noreply.github.com>
2026-09-25 15:37:09 +0530

    chore: clean up temporary scripts

--- fix_r2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let r = fs.readFileSync('backend/src/routes/worker.routes.ts', 'utf8');
-r = r.replace(
-  'getMonthlyReport',
-  'getMonthlyReport, getWorkerHistory'
-);
-fs.writeFileSync('backend/src/routes/worker.routes.ts', r);
